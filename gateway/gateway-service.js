20a64be1b65459d97a2bca9347cbcb583a0e16e7
AndreaAcero <158502529+AndreaAcero@users.noreply.github.com>
2026-04-04 13:43:58 +0200

    cambios cors

--- gateway/gateway-service.js
+++ gateway/gateway-service.js
@@ -789,7 +789,7 @@ const server = app.listen(port, () => {});
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
   },
 });
